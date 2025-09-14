726db190537eaa8c87a0a23fcb6266f3455b0536
vivy33 <vivymomo0407@gmail.com>
2025-09-14 19:02:28 +0800

    license

--- src/utils/rbtree.c
+++ src/utils/rbtree.c
@@ -1,9 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
+  Red Black Trees
+  (C) 1999  Andrea Arcangeli <andrea@suse.de>
+  (C) 2002  David Woodhouse <dwmw2@infradead.org>
+  (C) 2012  Michel Lespinasse <walken@google.com>
+
+
   linux/lib/rbtree.c
 */
-
-//#include <linux/rbtree_augmented.h>
-//#include <linux/export.h>
 
 /*------------------------------------- I add ----------------------------------------------*/
 
