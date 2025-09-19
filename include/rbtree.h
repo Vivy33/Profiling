776579d409cb6c41eeee58903fb8eee19de10da6
vivy33 <vivymomo0407@gmail.com>
2025-09-20 03:05:26 +0800

    style(rbtree): 修正文件格式
    - 修正 container_of 宏定义的代码缩进。
    - 在文件末尾添加换行符，以符合编码规范。

--- include/rbtree.h
+++ include/rbtree.h
@@ -30,7 +30,7 @@
 
 #define container_of(ptr, type, member) ({                      \
     const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-    (type *)( (char *)__mptr - offsetof(type,member) );})
+    (type *)( (char *)__mptr - offsetof(type,member) );}) 
 
 /*------------------ end ----------------------------*/
 
