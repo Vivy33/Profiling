6beb496f73e6bb5808e36186802f930f74c0a59e
vivy33 <vivymomo0407@gmail.com>
2025-09-27 14:37:23 +0800

    refactor(database): 更新 db_writer_init 函数签名以使用 struct profiling_config

--- include/database.h
+++ include/database.h
@@ -21,7 +21,7 @@ typedef struct db_writer_context_t db_writer_context_t;
  * @param db_path 要打开或创建的数据库文件的路径。
  * @return 成功时返回一个指向 db_writer_context_t 的指针，失败时返回 NULL。
  */
-db_writer_context_t* db_writer_init(const profiling_config_t *config);
+db_writer_context_t* db_writer_init(const struct profiling_config *config);
 
 /**
  * @brief 启动数据库后台写入线程。
