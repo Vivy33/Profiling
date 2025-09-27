b7859ef6153bd4886bd671152444e59de5ac07dd
vivy33 <vivymomo0407@gmail.com>
2025-09-27 14:38:17 +0800

    refactor(core): 移除 main_loop.c 中未使用的头文件

--- src/core/main_loop.c
+++ src/core/main_loop.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <errno.h>
-// #include <sys/time.h>
 
 #include "../../include/header.h"
 #include "../../include/config.h"
