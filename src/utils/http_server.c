a552fd893dedd1b5e29ad1446cde94f84b38c94f
vivy33 <vivymomo0407@gmail.com>
2025-10-04 03:08:27 +0800

    fix(http_server): 修正头文件包含路径
    更新了 http_server.h 和 database.h 的头文件包含路径，以适应新的项目结构。

--- src/utils/http_server.c
+++ src/utils/http_server.c
@@ -9,8 +9,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include "../../include/http_server.h"
-#include "../../include/database.h" // 包含 db_writer_get_db_handle
+#include "include/http_server.h"
+#include "include/database.h" // 包含 db_writer_get_db_handle
 
 // 说明：为了解决缺少网络相关符号（sockaddr_in/htons/htonl/INADDR_LOOPBACK）导致的编译错误，
 // 我们显式引入 sys/socket.h、netinet/in.h、arpa/inet.h 头文件；
